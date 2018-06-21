05ddaf6582eada3f9fa57dc585b96e36d567e41d
Jianhua Zhao <jiazhao@Jianzhao.local>
2018-06-20 21:37:14 -0700

    init

---
+++ src/main/java/org/ootb/service/PBService.java
@@ -0,0 +1,15 @@
+package org.ootb.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jiazhao on 2/5/18.
+ */
+
+@Service
+public class PBService {
+
+    public int[] generate(int max){
+        return null;
+    }
+}
